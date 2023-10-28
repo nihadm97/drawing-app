73d0e89a386451a8acc919170f03156f2023329b
nihadm97 <nihadm97@outlook.com>
2023-10-28 19:32:01 +0200

    first commit

--- src/Canvas.js
+++ src/Canvas.js
@@ -7,7 +7,6 @@ import {Link} from "react-router-dom";
 
 export const Canvas = () => {
   let db = firebase.firestore();
-
   const [isDrawing, setIsDrawing] = useState(false);
   const [tool, setTool] = useState("handdrawn");
   const [tool1, setTool1] = useState("#000000");
@@ -25,15 +24,16 @@ export const Canvas = () => {
   
   const prepareCanvas = () => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight-140;
+    canvas.width = 500;
+    canvas.height = 500;
+    canvas.style.backgroundColor = "lightblue";
     const context = canvas.getContext("2d");
-    context.lineWidth = 5;
+    context.lineWidth = 3;
     contextRef.current = context;
     const image = new Image();
     image.src = img
     image.onload = () => {
-    context.drawImage(image, 0, 0, window.innerWidth, window.innerHeight-140);
+    context.drawImage(image, 0, 0, 500, 500);
     };
   };
 
@@ -84,9 +84,36 @@ export const Canvas = () => {
   const clearCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    context.fillStyle = "white";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "lightblue";
+    context.fillRect(0, 0, 500, 500);
   }
+
+  function rotate(e){
+    e.preventDefault();
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    var mCanvas=document.createElement('canvas');
+    mCanvas.width=canvas.width;
+    mCanvas.height=canvas.height;
+    var mctx=mCanvas.getContext('2d');
+  
+    mctx.drawImage(canvas,0,0);
+  
+    context.clearRect(0,0,canvas.width,canvas.height);
+
+    context.translate(250,250);
+  
+    var radians=90/180*Math.PI;
+    context.rotate(radians);
+  
+    context.drawImage(mCanvas,-canvas.width/2,-canvas.height/2);
+  
+    context.rotate(-radians);
+    context.translate(-canvas.width/2,-canvas.height/2);
+  
+  }
+
   useEffect(() => {
     prepareCanvas();
   }, [img]);
@@ -136,7 +163,7 @@ function getImage(e) {
     } 
   }
   image.onload = () => {
-  context.drawImage(image, 0, 0, window.innerWidth, window.innerHeight-140);
+  context.drawImage(image, 0, 0, 500, 500);
   }; 
 }
 useEffect(() => {
@@ -151,46 +178,57 @@ useEffect(() => {
   });
 }, [])
   return (
-    <div className="container" style={{marginLeft: -135}}>
-    <div style={{backgroundColor: 'lightblue', border: '2px solid black', borderRadius: 10, padding: 10, width: window.innerWidth- 20, marginTop: 5}}>
+    <div>
+      <br></br>
+      <div className="row align-items-end" style={{ backgroundColor: 'lightblue', border: '2px solid black', borderRadius: 5 }}>
+  <div className="form-group col-12 col-md-6 col-sm-12 col-xs-12">
     <CompactPicker color={tool1} onChangeComplete={changeColor} />
-    <input type="file" style={{color: "transparent", marginLeft: 5}} onChange={(e) => {if(e.target.files[0]!=undefined) {setImg(URL.createObjectURL(e.target.files[0])) }}} />
-    <Link to="/galery"><button className="form-control" style={{width:"5%", marginLeft: 250, marginTop: -5}}>Galery</button></Link>
-    <form>
-    <input type="text" className="form-control" style={{marginTop: -87, marginLeft: 1100, width:"10%"}} value={name} onChange={e => setName(e.target.value)} required />
-    <button className="form-control" onClick={addImage} style={{width:"5%", marginLeft: 1235, marginTop: -7}}>Save</button>
+  </div>
+  <div className="form-group col-12 col-md-6 col-sm-12 col-xs-12">
+    <form className="form-inline">
+      <input type="text" className="form-control" style={{ width: "30%" }} value={name1} onChange={e => setName1(e.target.value)} required />
+      <button className="form-control" style={{ width: "30%" }} onClick={getImage}>Get</button>
     </form>
-    <form>
-    <input type="text" className="form-control" style={{marginTop: 7, marginLeft: 1100, width:"10%"}} value={name1} onChange={e => setName1(e.target.value)} required />
-    <button className="form-control" onClick={getImage} style={{width:"5%", marginTop: 40, marginLeft: 1235}}>Get</button>
+    <input type="file" style={{ color: "transparent" }} onChange={(e) => { if (e.target.files[0] != undefined) { setImg(URL.createObjectURL(e.target.files[0])) } }} />
+    <Link to="/galery"><button className="form-control" style={{ width: "40%" }}>Gallery</button></Link>
+  </div>
+  <div className="form-group col-12">
+    <form className="form-inline">
+      <input type="text" className="form-control" style={{ width: "50%" }} value={name} onChange={e => setName(e.target.value)} required />
+      <button className="form-control" onClick={addImage} style={{ width: "30%" }}>Save</button>
     </form>
-    <div className="form-group" style={{float: "right", marginTop: -87, marginRight: 270}}>
-    <form class="form-inline">
-    <label className="form-control">Selection:</label>
-    <label className="form-control" htmlFor="selection">Hand drawn</label>
-    <input className="form-control" type="radio" id="selection1" checked={tool === "handdrawn"} onChange={() => setTool("handdrawn")}/> 
-    <label className="form-control" htmlFor="selection">Line</label>
-    <input className="form-control" type="radio" id="selection2" checked={tool === "line"} onChange={() => setTool("line")}/> 
-    <label className="form-control" htmlFor="selection">Rectangle</label>
-    <input className="form-control" type="radio" id="selection3" checked={tool === "rectangle"} onChange={() => setTool("rectangle")}/>
-    <label className="form-control" htmlFor="selection">Circle</label><br></br><br></br>
-    <input className="form-control" type="radio" id="selection4" checked={tool === "circle"} onChange={() => setTool("circle")}/>
-    <label className="form-control" style={{marginLeft: 10}}>Style:</label>
-    <label className="form-control" htmlFor="style">Full</label>
-    <input className="form-control" type="radio" id="style1" checked={tool2 === "full"} onChange={() => setTool2("full")}/>
-    <label className="form-control" htmlFor="style">Empty</label> 
-    <input className="form-control" type="radio" id="style2" checked={tool2 === "empty"} onChange={() => setTool2("empty")}/> 
+    <form className="form-inline">
+      <label className="form-control">Selection:</label>
+      <label className="form-control" htmlFor="selection">Hand drawn</label>
+      <input className="form-control" type="radio" id="selection1" checked={tool === "handdrawn"} onChange={() => setTool("handdrawn")} />
+      <label className="form-control" htmlFor="selection">Line</label>
+      <input className="form-control" type="radio" id="selection2" checked={tool === "line"} onChange={() => setTool("line")} />
+      <label className="form-control" htmlFor="selection">Rectangle</label>
+      <input className="form-control" type="radio" id="selection3" checked={tool === "rectangle"} onChange={() => setTool("rectangle")} />
+      <label className="form-control" htmlFor="selection">Circle</label><br /><br />
+      <input className="form-control" type="radio" id="selection4" checked={tool === "circle"} onChange={() => setTool("circle")} />
     </form>
-    </div>
-    <button className="form-control" style={{marginLeft: 370, width:"10%", marginTop: 40}} onClick={clearCanvas}>Clear</button>
-    </div>
+    <form className="form-inline">
+      <label className="form-control">Style:</label>
+      <label className="form-control" htmlFor="style">Full</label>
+      <input className="form-control" type="radio" id="style1" checked={tool2 === "full"} onChange={() => setTool2("full")} />
+      <label className="form-control" htmlFor="style">Empty</label>
+      <input className="form-control" type="radio" id="style2" checked={tool2 === "empty"} onChange={() => setTool2("empty")} />
+      <button className="form-control" onClick={rotate} style={{ width: "30%" }}>Rotate</button>
+    </form>
+  </div>
+</div>
+ 
+    <br></br>
     <canvas
       onMouseDown={startDrawing}
       onMouseUp={finishDrawing}
       onMouseMove={draw}
       ref={canvasRef}
     />
+    <button className="form-control" onClick={clearCanvas} style={{width: "10%"}}>Clear</button>
     </div>
+    
   );
 };
 
